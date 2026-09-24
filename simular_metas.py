b5a7c2153540e1562413b51d8e2b2bb5e13d7100
Dadzea <dadzea99@gmail.com>
2026-09-23 20:38:32 -0500

    Feat/ Se construye funcion para generar los datos de usuario y metas , cada uno verificando sus atributos en la historia de usuario de cada uno

--- simular_metas.py
+++ simular_metas.py
@@ -24,3 +24,28 @@ random.seed(42)
 
 #4. Definir el numero de datos simulados (DATASET)
 FILAS=500
+
+# 5 Construir funcion generadora de datos simulados
+
+estados=["pendiente", "en progreso" ,"completada"]
+ids_empresa=["Apple", "Microsoft", "Google", "Amazon", "Facebook"]
+ids_categoria=["Desarrollo", "Marketing", "Ventas", "Soporte"]
+ids_prioridad=["Alta", "Media", "Baja"]
+
+
+def generar_datos_metas(numero_registros=FILAS):
+
+    filas=[]
+    for _ in range(numero_registros):
+        filas.append({
+            "id": str(uuid.uuid4()),
+            "nombre": fake.sentence(nb_words=6).rstrip("."),
+            "descripcion": fake.sentence(nb_words=12),
+            "fecha_inicio": fake.date_between(start_date="-1y", end_date="+3m"),
+            "fecha_fin": fecha_inicio + timedelta(days=random.randint(15, 180)),
+            "estado": random.choice(estados),
+            "id_empresa":random.choice(ids_empresa),
+            "id_categoria": random.choice(ids_categoria),
+            "id_prioridad": random.choice(ids_prioridad)
+        })
+    return filas
--- simular_usuario.py
+++ simular_usuario.py
@@ -21,3 +21,29 @@ random.seed(42)
 
 #4. Definir el numero de datos simulados (DATASET)
 FILAS=400
+
+
+
+# 5  Construir funcion generadora de datos simulados
+
+ROLES=[
+    "administrador",
+    "empresario",
+    "profesor"
+]
+
+
+def generar_datos_usuarios(numero_registros=FILAS):
+
+    filas=[]
+    for _ in range(numero_registros):
+        filas.append({
+            "id": str(uuid.uuid4()),
+            "nombre": fake.name(),
+            "correo": fake.email(),
+            "contrasena_hash": fake.sha256(),
+            "rol": random.choice(ROLES),
+            "activo": random.choice([True, False]),
+            "fecha_registro": fake.date_time_between(start_date="-2y", end_date="now")
+        })
+    return filas
